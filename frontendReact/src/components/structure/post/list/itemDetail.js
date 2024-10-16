caa7ef56020666948d2e4d615e943bc4d024ed66
andresGooz <gdegooz@gmail.com>
2024-10-16 20:43:48 -0300

    feat: add edit link in post detail

--- frontendReact/src/components/structure/post/list/itemDetail.js
+++ frontendReact/src/components/structure/post/list/itemDetail.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router-dom';
 
 function ItemDetail({ post }) {
   return (
-    <li key={post.id} className="mdc-list-item" tabIndex="0">
-      <Link to={`/post/${post.id}`}>
-        <span className="mdc-list-item__text">{post.id}</span><br />
-        <span className="mdc-list-item__text">{post.name}</span><br />
-        <span className="mdc-list-item__text">{post.description}</span>
+    <>
+      <li key={post.id} className="mdc-list-item" tabIndex="0">
+        <Link to={`/post/${post.id}`}>
+          <span className="mdc-list-item__text">{post.id}</span><br />
+          <span className="mdc-list-item__text">{post.name}</span><br />
+          <span className="mdc-list-item__text">{post.description}</span>
+        </Link>
+      </li>
+      <Link to={`/post/edit/${post.id}`}>
+              <md-filled-button>
+                <md-icon class="material-icons">edit</md-icon>
+                Edit Post
+              </md-filled-button>
       </Link>
-    </li>
+    </>
   );
 }
 
