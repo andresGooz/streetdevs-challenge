81a845bd18961458c77ce391ed4296dabcf90578
andresGooz <gdegooz@gmail.com>
2024-10-16 20:47:26 -0300

    feat: add dummy save button

--- frontendReact/src/components/structure/post/form/editPost.js
+++ frontendReact/src/components/structure/post/form/editPost.js
@@ -4,13 +4,19 @@ import { Link } from 'react-router-dom';
 
 function EditPostForm({ post }) {
   return (
-    <li key={post.id} className="mdc-list-item" tabIndex="0">
-      <Link to={`/post/${post.id}`}>
-        <span className="mdc-list-item__text">{post.id}</span><br />
-        <span className="mdc-list-item__text">{post.name}</span><br />
-        <span className="mdc-list-item__text">{post.description}</span>
-      </Link>
-    </li>
+    <>
+        <li key={post.id} className="mdc-list-item" tabIndex="0">
+        <Link to={`/post/${post.id}`}>
+            <span className="mdc-list-item__text">{post.id}</span><br />
+            <span className="mdc-list-item__text">{post.name}</span><br />
+            <span className="mdc-list-item__text">{post.description}</span>
+        </Link>
+        </li>
+        <md-filled-button type="submit">
+            <md-icon class="material-icons">save</md-icon>
+            Save Post
+        </md-filled-button>
+    </>
   );
 }
 
